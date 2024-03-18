74a74f709ce99f6dec90071619e750354fc29eae
Dima <dimasalabay0711@gmail.com>
2024-03-18 16:34:53 +0200

    done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,79 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+import HttpError from "../helpers/HttpError.js";
+import validateBody from "../helpers/validateBody.js";
 
-export const getOneContact = (req, res) => {};
+import {
+	createContactSchema,
+	updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const deleteContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+	try {
+		const result = await contactsService.getAllContacts();
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
 
-export const createContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const result = await contactsService.getContactById(id);
+		if (!result) {
+			throw HttpError(404, `Contact with id = ${id} not found`);
+		}
 
-export const updateContact = (req, res) => {};
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const deleteContact = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const result = await contactsService.removeContact(id);
+		if (!result) {
+			throw HttpError(404, `Contact with id = ${id} not found`);
+		}
+
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const createContact = async (req, res, next) => {
+	try {
+		// validateBody(createContactSchema);
+		const { error } = createContactSchema.validate(req.body);
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+		const result = await contactsService.addContact(req.body);
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const updateContact = async (req, res, next) => {
+	try {
+		const { error } = updateContactSchema.validate(req.body);
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+		const { id } = req.params;
+		const result = await contactsService.updateContactById(id, req.body);
+
+		if (!result) {
+			throw HttpError(404, `Contact with id = ${id} not found`);
+		}
+
+		res.json(result);
+	} catch (error) {
+		next(error);
+	}
+};
